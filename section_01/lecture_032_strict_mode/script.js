94c2bed47b37b58861353c83f39b52ec5c599e16
JonasFiechter <jonasrfiechter@gmail.com>
2022-02-22 19:53:12 -0300

    one more lecture :D

---
+++ section_01/lecture_032_strict_mode/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('TESTING')
+console.log('TESTING')
+console.log('TESTING')
+
+let has_drivers_license = false;
+const pass_test = true;
+
+if (pass_test) has_driver_license = true
+
+if (has_drivers_license) console.log('I can drive :D')
