4b0ee776cf5fe9f4c6cd01dff07ad6f6b88fd74a
JonasFiechter <jonasrfiechter@gmail.com>
2022-03-01 18:42:10 -0300

    more lectures

---
+++ section_03/lecture_039_arrays/script.js
@@ -0,0 +1,8 @@
+const friend_1 = 'Michael';
+const friend_2 = 'Steven';
+const friend_3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends)
+
+const years = new Array(1991, 'Test', 233, friend_1)
---
+++ section_03/lecture_040_array_operations/script.js
@@ -0,0 +1,6 @@
+const friends = ['Michael', 'Steven', 'Peter'];
+
+friends.push('Jay')
+friends.unshift('Rebbeca')
+
+console.log(friends)
