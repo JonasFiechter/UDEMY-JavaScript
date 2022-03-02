347cd2f1b2184893daa9a77795a9d1c9808bf4cc
JonasFiechter <jonasrfiechter@gmail.com>
2022-03-02 14:59:23 -0300

    more and more lectures

---
+++ section_03/lecture_046_for/script.js
@@ -0,0 +1,8 @@
+/* 
+The for loop
+*/
+
+// FOR loop keeps running while this condition is TRUE
+for(let rep = 1; rep <= 10; rep++) {
+    console.log(rep)
+}
---
+++ section_03/lecture_047_looping_arrays/script.js
@@ -0,0 +1,29 @@
+const jonas_array = [
+    'Jonas',
+    'Fiechter',
+    2037 - 1993,
+    ['Michael', 'Peter', 'Steven'],
+    true,
+    'something else'
+];
+
+const types = []
+
+for(let i = 0; i < jonas_array.length; i++) {
+    console.log(jonas_array[i], typeof(jonas_array[i]));
+
+    //Filling types array
+    // types[i] = typeof jonas_array[i];
+
+    //other way to fill types array
+    types.push(typeof(jonas_array[i]))
+}
+
+console.log(types)
+
+const years = [1991, 2007, 1969, 2020];
+const ages = []
+
+for (let i = 0; i < years.length; i++) {
+    console.log(years[i])
+}
