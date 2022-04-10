5ef771bc9b5f3e3a31b547c25a780af6ee877b8d
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-10 10:25:53 -0300

    new lecture

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict;'
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starter_menu: ['Foccacia', 'Bruschetta', 'Garlic bread', 'Salada capresi'],
+    main_menu: ['Pizza', 'Pasta', 'Risotto']
+}
