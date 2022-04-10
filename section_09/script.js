db01efdc5100d9ec46f925002f07e694f788e3e5
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-10 16:42:23 -0300

    new lecture

--- section_09/script.js
+++ section_09/script.js
@@ -5,5 +5,11 @@ const restaurant = {
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starter_menu: ['Foccacia', 'Bruschetta', 'Garlic bread', 'Salada capresi'],
-    main_menu: ['Pizza', 'Pasta', 'Risotto']
+    main_menu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (first_item, second_item) {
+        return [this.starter_menu[first_item], this.main_menu[second_item]]
+    }
 }
+
+const [a, b] = restaurant.order(1, 2)
+console.log(a, b)
