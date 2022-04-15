9192f215ca69b315c2617186ff5522df44e38b4d
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-15 10:56:27 -0300

    update
    new lecture

---
+++ section_09/the_spread_operator/script.js
@@ -0,0 +1,12 @@
+'use strict;'
+
+/* 
+The spread operator '...' is very usefull in unpacking objects, arrays and others
+iterables.
+*/
+
+const arr = [7, 8, 9];
+const bad_new_arr = [1, 2, 7, 8, 9];
+const good_new_array = [1, 2, ...arr];
+
+console.log(bad_new_arr, good_new_array)
