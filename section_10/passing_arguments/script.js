91135a7c6fc816564fb85d43b01cfc1699dd8d42
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-20 15:57:17 -0300

    update
    more lectures

--- section_10/passing_arguments/script.js
+++ section_10/passing_arguments/script.js
@@ -1,19 +1,33 @@
 'use strict'
 
-const bookings = [];
-
-//                                          defined 1     defined 199
-const createBooking = function(flightNum, passengersNum=1, price=199) {
-    const booking = {
-        flightNum,
-        passengersNum,
-        price
-    }
-    bookings.push(booking)
+const flight = 'LH234'
+const jonas = {
+    name: 'Jonas Fiechter',
+    passport: 1238383838
 };
 
-createBooking('LH123', 44, 994)
-createBooking('DD232', undefined, 333)
-createBooking('XT123', 94, 424)
 
-console.log(bookings)
+function checkIn(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if(passenger.passport === 1238383838){
+        alert('Check in')
+    } else {
+        alert('Wrong passport')
+    }
+}
+
+// checkIn(flight, jonas);
+// console.log(flight, jonas)
+
+// // Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+function newPassport(person) {
+    person.passport = Math.trunc(Math.random() * 100000000000)
+}
+
+newPassport(jonas);
+checkIn(flight, jonas);
