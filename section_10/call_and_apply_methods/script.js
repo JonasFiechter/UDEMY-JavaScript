bd17dcc549bab06b0042d3ddbeabebe724264cbb
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-21 11:25:17 -0300

    update
    .

---
+++ section_10/call_and_apply_methods/script.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    //book: () {} <-- Old version
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline}
+        flight ${this.iataCode}${flightNum}`)
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+    }
+};
+lufthansa.book(239, 'Jonas Fiechter');
+lufthansa.book(531, 'Mike Smith');
+
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+}
+
+const book = lufthansa.book
+
+// Does NOT work
+// book(132, 'Sarah Williams');
+
+
+// Call method
+// Call the function with the first argument as the 'this.' keyword
+book.call(eurowings, 243, 'Sarah Williams')
+
+
+const swiss = {
+    airline: 'Swiss',
+    iataCode: 'LX',
+    bookings: []
+}
+
+book.call(swiss, 594, 'Mary Cooper')
+
+
+// Apply Method
+const flightData = [572, 'George Cooper'];
+const flightData2 = [612, 'Sarah Cooper'];
+book.apply(lufthansa, flightData);
+
+book.call(lufthansa, ...flightData2);
+
+console.log(lufthansa);
+console.log(eurowings);
+console.log(swiss);
