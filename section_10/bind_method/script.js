4c3cdc003148d0f92da7c40b91feba9fff4635a4
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-26 14:03:14 -0300

    new lecture bind method ! That's tuff

--- section_10/bind_method/script.js
+++ section_10/bind_method/script.js
@@ -46,9 +46,58 @@ book.call(lufthansa, ...flightData2);
 
 // Bind Method
 const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
 
 bookEW(32, 'Stephen Hawking');
+bookLX(25, 'Martha cooper');
 
 console.log(lufthansa);
 console.log(eurowings);
 console.log(swiss);
+
+// With Event listners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+    console.log(this)
+    this.planes++
+    console.log(this.planes)
+};
+
+// The "".this" keyword of the METHOD passed into the addEventListner('METHOD') will be set with
+// the higher level function returned object like below, .this --> '.buy' button
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
+
+// To fix this we can use Bind method .bind to change the '.this' value
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+// The first argument of bind method is '.this' but here we don't need it so place 'null' instead
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(200));
+
+// Challenge create a function that returns another one with the tax value and rate
+// My solution
+const addTax2 = function(rate) {
+    return function(value) {
+        console.log(value + value * rate)
+    }
+}
+
+const taxRate = addTax2(0.1)
+taxRate(400)
+
+// Professor solution
+const addTaxRate = function(rate) {
+    return function(value) {
+        return value + value * rate
+    }
+}
+
+const addVAT2 = addTaxRate(0.23)
+console.log(addVAT2(100));
+console.log(addVAT2(23));
