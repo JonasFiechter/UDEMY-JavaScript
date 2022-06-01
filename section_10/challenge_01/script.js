688ae0f3272243fd0a46220f3c83d48f42387a07
JonasFiechter <jonasrfiechter@gmail.com>
2022-06-01 16:24:14 -0300

    update
    this keyword now working

--- section_10/challenge_01/script.js
+++ section_10/challenge_01/script.js
@@ -28,17 +28,15 @@ sense (e.g. answer 52 wouldn't make sense, right?)
 3. Create a method 'displayResults' which displays the poll results. The
 method takes a string as an input (called 'type'), which can be either 'string'
 or 'array'. If type is 'array', simply display the results array as it is, using
-console.log(). a
-string like "Poll results are 13, 2, 4, 1"This should be the default option. If type is 'string', 
-display .
+console.log(). a string like "Poll results are 13, 2, 4, 1"This should be the default option. 
+If type is 'string', display .
 
 4. Run the 'displayResults' method at the end of each
 'registerNewAnswer' method call.
 
 5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
 data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
-object! So what should the this keyword look like in this situation?The Complete JavaScript Course
-21
+object! So what should the this keyword look like in this situation?
 Test data for bonus:
 § Data 1: [5, 2, 3]
 § Data 2: [1, 5, 3, 9, 6, 1]
@@ -49,31 +47,28 @@ GOOD LUCK 😀
 const poll = {
     question: "What is your favourite programming language?",
     options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-    // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
-    registerNewAnswer: () => {
-        const answer = prompt('What is your favourite programming language?\n\
-            0: JavaScript \
-            1: Python \
-            2: Rust \
-            3: C++')
+    registerNewAnswer() {
+        const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}`))
         if (answer < 5) {
-            poll.answers[answer] ++
-            poll.displayResults()
+            this.answers[answer] ++
+            this.displayResults()
         }
     },
-    displayResults: () => {
-        const result = `JavaScript: ${poll.answers[0]} | Python: ${poll.answers[1]} | Rust: \
-                                   ${poll.answers[2]} | C++: ${poll.answers[3]}`
+    displayResults() {
+        const result = `JavaScript: ${this.answers[0]} | Python: ${this.answers[1]} | Rust: \
+                                   ${this.answers[2]} | C++: ${this.answers[3]}`
 
         results.textContent = result
     }
 };
 
+
 const questionBtn = document.querySelector('.question')
 const resultsBtn = document.querySelector('.results')
 const results = document.querySelector('.show-results')
 
-questionBtn.addEventListener('click', poll.registerNewAnswer)
-resultsBtn.addEventListener('click', poll.displayResults)
+questionBtn.addEventListener('click', poll.registerNewAnswer.bind(poll))
+resultsBtn.addEventListener('click', poll.displayResults.bind(poll))
 
+poll.displayResults.call({answers: data1})
