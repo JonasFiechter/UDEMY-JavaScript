d3f8e437958a3fe1d11dcccf808b9a4813a99362
JonasFiechter <jonasrfiechter@gmail.com>
2022-07-26 19:03:03 -0300

    lecture done
    .

--- section_10/working_with_strings/script.js
+++ section_10/working_with_strings/script.js
@@ -18,8 +18,28 @@ const capitalizeName = function(name) {
     const capitalizeNameSplited = []
     for (const n of splitedName) {
         capitalizeNameSplited.push(n[0].toUpperCase() + n.slice(1))
+        // alternative way of doing it
+        // capitalizeNameSplited.push(n[0].replace, n[0].toUpperCase())
     }
     return capitalizeNameSplited.join(' ')
 }
 
 console.log(capitalizeName('maria markell santos'))
+
+
+// Padding
+const message = 'Go for launch!'
+console.log(message.padStart(25, '%'))
+
+// CreditCard masking
+const maskNumber = function(number) {
+    const str = number + '' //transform any input to string
+    const masked = ''.padStart((str.length - 4), '*') + str.slice(-4)
+    console.log(masked)
+}
+
+maskNumber(120391209312)
+
+// Repeat
+const message2 = 'Bad weather, ALL DEPARTURE DELAYED';
+console.log(message2.repeat(23))
