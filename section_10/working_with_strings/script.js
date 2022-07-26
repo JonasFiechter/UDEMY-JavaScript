1f116e5a3a504622f1efd51b9adc11d309887ef0
JonasFiechter <jonasrfiechter@gmail.com>
2022-07-26 18:32:19 -0300

    update
    function done

--- section_10/working_with_strings/script.js
+++ section_10/working_with_strings/script.js
@@ -1,10 +1,25 @@
 'use strict'
 
+// Split method
 const myName = 'Jonas Schmedtman'
 console.log(myName.split(' '))
 
 const [firstName, lastName] = myName.split(' ')
 console.log(firstName, lastName)
 
+// Join method
 const formalName = ['Mr', firstName, lastName].join(' ')
 console.log(formalName)
+
+
+// function using slice, split and join to capitaliza any name
+const capitalizeName = function(name) {
+    const splitedName = name.split(' ')
+    const capitalizeNameSplited = []
+    for (const n of splitedName) {
+        capitalizeNameSplited.push(n[0].toUpperCase() + n.slice(1))
+    }
+    return capitalizeNameSplited.join(' ')
+}
+
+console.log(capitalizeName('maria markell santos'))
